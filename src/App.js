358c539ef1438090bcaa3fad14a522229367c742
ankit sharma <126191725+ankit12sharm@users.noreply.github.com>
2023-09-07 16:04:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+// import logo from './logo.svg';
+import './App.css';
+import Weather from './components/currentLocation';
+
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Weather />
+      </div>
+      {/* <div className="footer-info">
+        <a href="/">
+          
+        </a>{" "}
+         {" "}
+        <a target="_blank" href="/">
+          
+        </a>{" "}
+        | {" "}
+        <a target="_blank" href="/">
+        
+        </a>
+      </div> */}
+    </>
+  );
+}
+
+export default App;
